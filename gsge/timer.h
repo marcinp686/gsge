1ab5eb60e225225c234696dfc993e75c927c8e26
Marcin Plichta <marcinp689@gmail.com>
2022-10-12 21:15:44 +0200

    Added printTimer function to temporarily benchmark functions

--- gsge/timer.h
+++ gsge/timer.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <chrono>
+#include <spdlog/spdlog.h>
 
 class timer
 {
   public:
     timer();
     float resetTimer();
+    void printTimer();
 
   private:
     std::chrono::steady_clock::time_point lastTimer;
