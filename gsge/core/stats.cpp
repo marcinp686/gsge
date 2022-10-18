18e8bd0ce425945a1aebecbe81f3a5290be1a64a
Marcin Plichta <marcinp689@gmail.com>
2022-10-18 23:59:34 +0200

    Added min/max fps to log

--- gsge/core/stats.cpp
+++ gsge/core/stats.cpp
@@ -8,7 +8,7 @@ void stats::update()
     if (averageFrameTimeCounter == averageFrameCountNumber)
     {
         currentFps = 1.0f / (totalFrameTimeCounter / averageFrameCountNumber);
-        spdlog::info("{:.1f}", currentFps);
+        spdlog::info("FPS {:.1f}\tMIN {:.1f}\tMAX {:.1f}", currentFps, minFps, maxFps);
         if (currentFps > maxFps)
             maxFps = currentFps;
         if (currentFps < minFps)
