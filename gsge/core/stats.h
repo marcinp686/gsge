cac7a513a271776fd30c5ece43ff11734e16045a
Marcin Plichta <marcinp689@gmail.com>
2022-10-22 15:24:45 +0200

    Modified initial frame count number

--- gsge/core/stats.h
+++ gsge/core/stats.h
@@ -11,13 +11,13 @@ class stats
 
     float dt = 0.0f;
 
-    size_t averageFrameCountNumber = 400;
+    size_t averageFrameCountNumber = 50;
     size_t averageFrameTimeCounter = 0;
     float totalFrameTimeCounter = 0.0f;
 
     float maxFps = 0.0f;
     float currentFps = 0.0f;
-    float minFps = std::numeric_limits<float>::max();
+    float minFps = 10000.0f;
 
   private:
     timer frameTime;
