b4c0fca6da6fcb1adb815706da75e57454ed4be3
Marcin Plichta <marcinp689@gmail.com>
2023-07-07 16:19:03 +0200

    Modified stats to ignore first 2 seconds or app running

--- gsge/core/stats.cpp
+++ gsge/core/stats.cpp
@@ -2,17 +2,23 @@
 
 void stats::update()
 {
+    frameNumber++;
+
     dt = frameTime.resetTimer();
+    if (totalRunningTime.getTimeAsSeconds()<2) return;
+    
     averageFrameTimeCounter++;
     totalFrameTimeCounter += dt;
     if (averageFrameTimeCounter == averageFrameCountNumber)
     {
         currentFps = 1.0f / (totalFrameTimeCounter / averageFrameCountNumber);
-        SPDLOG_INFO("FPS {:.1f}\tMIN {:.1f}\tMAX {:.1f}", currentFps, minFps, maxFps);
+        
         if (currentFps > maxFps)
             maxFps = currentFps;
         if (currentFps < minFps)
             minFps = currentFps;
+
+        SPDLOG_INFO("FPS {:.1f}\tMIN {:.1f}\tMAX {:.1f}", currentFps, minFps, maxFps);
 
         averageFrameTimeCounter = 0;
         totalFrameTimeCounter = 0;
